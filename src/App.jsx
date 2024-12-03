17a68a2d064678a6bff70b8b99684e11be07bef3
nnoskov <hhb@bk.ru>
2024-12-03 23:07:00 +0300

    remove button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react';
 import './App.css';
 
 export const App = () => {
-	const [count, setCount] = useState(0);
 	return (
 		<>
 			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-			</div>
 		</>
 	);
 };
