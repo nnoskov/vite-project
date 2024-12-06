69bca5f9f299125ecdae04a97058b6605383232c
nnoskov <hhb@bk.ru>
2024-12-06 20:03:07 +0300

    autosetting of base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import fs from 'fs';
+import path from 'path';
+
+// eslint-disable-next-line no-undef
+const packageJsonPath = path.resolve(__dirname, 'package.json');
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/vite-project/',
+	base: `/${packageJson.name}/`,
 	plugins: [react(), svgr()],
 });
